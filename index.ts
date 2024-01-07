37b858b18d7d378820517e4cfd383fcb3272cb94
Rahaf0B <rahafbakeer123@gmail.com>
2024-01-07 19:27:49 +0200

    add index + more folders

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {Sequelize} from 'sequelize';
+
+const app = express();
+
+
+
+app.use("/",);
+
+app.use(async (req, res, next)=>{ 
+  res.status(404).send({message:"Not Found"});
+});
+
+app.listen(3000, () => {
+  console.log("Express app is listening on the port 3000!");
+});
