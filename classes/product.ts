9ea96690ac691876e009855e1c25ba17009a0af4
Rahaf0B <rahafbakeer123@gmail.com>
2024-01-15 20:31:14 +0200

    newArrival API

--- classes/product.ts
+++ classes/product.ts
@@ -6,6 +6,8 @@ import sequelizeConnection from "../conections/sequelizeConnection";
 import Image from "../models/Image";
 import Product_wishlist from "../models/product_wishlist";
 import Wishlist from "../models/Wishlist";
+import Rating from "../models/Rating";
+import Discount from "../models/Discount";
 
 export default class CProduct {
   private static instance: CProduct;
@@ -23,7 +25,7 @@ export default class CProduct {
   async getNewArrival(
     pageNumber: number,
     numberOFItems: number,
-    userId?: number,
+    userId?: number
   ): Promise<[IProduct[], number]> {
     try {
       const trans = await sequelizeConnection.sequelize.transaction();
@@ -47,9 +49,38 @@ export default class CProduct {
         transaction: trans,
       });
       const data = Product.findAll({
+        subQuery: false,
         offset: pageNumber === 0 ? 0 : (pageNumber - 1) * numberOFItems,
         limit: pageNumber === 0 ? 4 : numberOFItems,
-        attributes: ["product_id", "name", "sub_title", "price"],
+        attributes: [
+          "product_id",
+          "name",
+          "sub_title",
+          "price",
+          [
+            Sequelize.fn(
+              "SUM",
+              Sequelize.literal(
+                `DISTINCT CASE WHEN wishlist.normal_uid = ${
+                  userId ? userId : 0
+                } THEN 1 ELSE 0 END`
+              )
+            ),
+            "is_liked",
+          ],
+          [
+            Sequelize.fn("COUNT", Sequelize.col("rating.product_id")),
+            "number_of_ratings",
+          ],
+          [
+            Sequelize.fn("AVG", Sequelize.col("rating.value")),
+            "ratings",
+          ],
+          [
+            Sequelize.fn("SUM", Sequelize.col("discount.value")),
+            "discount_value",
+          ],
+        ],
         where: {
           [Op.and]: [
             Sequelize.where(
@@ -68,31 +99,32 @@ export default class CProduct {
           {
             model: Image,
             required: false,
-
+            nested: true,
             attributes: ["image_id", "name", "url"],
-            where: { type: { [Op.eq]: 1 } },
+            where: { type: { [Op.eq]: 1 
+            },   },
+            subQuery: false
           },
-          {
-            model: Product_wishlist,
-            required: false,
 
-           include: [
-{model: Wishlist, required: false, where:{normal_uid:userId ? 0 : userId}},
-           ]
-            
-          },
+          { model: Wishlist, required: false, attributes: [], subQuery: false },
+          { model: Rating, required: false, attributes: [], subQuery: false },
+          {model:Discount,required: false, attributes:[],subQuery: false  }
         ],
+        group: [
+          "product_id",
+          "image_id",
+          "wishlist.product_wishlist.wishlist_id","discount.product_discount.discount_id"
+        ], 
+
         transaction: trans,
       });
       try {
         const [products, count] = await Promise.all([data, countData]);
-
         return [products, count];
       } catch (e: any) {
         throw new Error(e.message);
       }
     } catch (e: any) {
-      console.log(e);
       throw new Error(e.message);
     }
   }
