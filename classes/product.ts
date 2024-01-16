86ee7331de22ea5e1204e712aecbdec6edc3f716
Rahaf0B <rahafbakeer123@gmail.com>
2024-01-16 14:35:46 +0200

    add  filter by category id and brand id

--- classes/product.ts
+++ classes/product.ts
@@ -22,13 +22,97 @@ export default class CProduct {
     return CProduct.instance;
   }
 
+  async getProductCount(conditionKey: string, conditionValue: string | number) {
+    try {
+      const countData = await Product.count({
+        where: {
+          [conditionKey]: conditionValue,
+        },
+      });
+      return countData;
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
+  }
+
+  async filterProductByCoB(
+    pageNumber: number,
+    numberOFItems: number,
+    userId: number,
+    conditionKey: string,
+    conditionValue: string | number
+  ) {
+    const countData = this.getProductCount(conditionKey, conditionValue);
+    const data = Product.findAll({
+      subQuery: false,
+      offset:  (pageNumber - 1) * numberOFItems,
+      limit:  numberOFItems,
+      attributes: [
+        "product_id",
+        "name",
+        "sub_title",
+        "price",
+        [
+          Sequelize.fn(
+            "SUM",
+            Sequelize.literal(
+              `DISTINCT CASE WHEN wishlist.normal_uid = ${
+                userId ? userId : 0
+              } THEN 1 ELSE 0 END`
+            )
+          ),
+          "is_liked",
+        ],
+        [
+          Sequelize.fn("COUNT", Sequelize.col("rating.product_id")),
+          "number_of_ratings",
+        ],
+        [Sequelize.fn("AVG", Sequelize.col("rating.value")), "ratings"],
+        [
+          Sequelize.fn("SUM", Sequelize.col("discount.value")),
+          "discount_value",
+        ],
+      ],
+      where: {
+        [conditionKey]: conditionValue,
+      },
+      include: [
+        {
+          model: Image,
+          required: false,
+          nested: true,
+          attributes: ["image_id", "name", "url"],
+          where: { type: { [Op.eq]: 1 } },
+          subQuery: false,
+        },
+
+        { model: Wishlist, required: false, attributes: [], subQuery: false },
+        { model: Rating, required: false, attributes: [], subQuery: false },
+        { model: Discount, required: false, attributes: [], subQuery: false },
+      ],
+      group: [
+        "product_id",
+        "image_id",
+        "wishlist.product_wishlist.wishlist_id",
+        "discount.product_discount.discount_id",
+      ],
+      order: [["product_id", "DESC"]],
+    });
+    try {
+      const [products, count] = await Promise.all([data, countData]);
+      return [products, count];
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
+  }
+
+
   async getNewArrival(
     pageNumber: number,
     numberOFItems: number,
     userId?: number
   ): Promise<[IProduct[], number]> {
     try {
-
       const created_at = new Date().setMonth(new Date().getMonth() - 3);
       const countData = Product.count({
         where: {
@@ -70,10 +154,7 @@ export default class CProduct {
             Sequelize.fn("COUNT", Sequelize.col("rating.product_id")),
             "number_of_ratings",
           ],
-          [
-            Sequelize.fn("AVG", Sequelize.col("rating.value")),
-            "ratings",
-          ],
+          [Sequelize.fn("AVG", Sequelize.col("rating.value")), "ratings"],
           [
             Sequelize.fn("SUM", Sequelize.col("discount.value")),
             "discount_value",
@@ -99,27 +180,26 @@ export default class CProduct {
             required: false,
             nested: true,
             attributes: ["image_id", "name", "url"],
-            where: { type: { [Op.eq]: 1 
-            },   },
-            subQuery: false
+            where: { type: { [Op.eq]: 1 } },
+            subQuery: false,
           },
 
           { model: Wishlist, required: false, attributes: [], subQuery: false },
           { model: Rating, required: false, attributes: [], subQuery: false },
-          {model:Discount,required: false, attributes:[],subQuery: false  }
+          { model: Discount, required: false, attributes: [], subQuery: false },
         ],
         group: [
           "product_id",
           "image_id",
-          "wishlist.product_wishlist.wishlist_id","discount.product_discount.discount_id"
-        ], 
+          "wishlist.product_wishlist.wishlist_id",
+          "discount.product_discount.discount_id",
+        ],
         order: [["product_id", "DESC"]],
       });
       try {
         const [products, count] = await Promise.all([data, countData]);
         return [products, count];
       } catch (e: any) {
-        console.log(e)
         throw new Error(e.message);
       }
     } catch (e: any) {
