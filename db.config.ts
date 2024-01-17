ebc462a2599d475fe6b74fb1080e20c7333b8379
Rahaf0B <rahafbakeer123@gmail.com>
2024-01-17 15:00:33 +0200

    update setting db

--- db.config.ts
+++ db.config.ts
@@ -1,19 +1,19 @@
-// export default {
-//     Host:"localhost",
-//     USER:"root",
-//     PASSWORD:"123456",
-//     DB:"finalpro",
-//     dialect:"mysql",
-
-// }
-
-
-
-
 export default {
-    Host: "sql12.freesqldatabase.com",
-    USER: "sql12677559",
-PASSWORD: "FVhPjz6YkK",
-DB:"sql12677559",
-Port: 3306
+    Host:"localhost",
+    USER:"root",
+    PASSWORD:"123456",
+    DB:"finalpro",
+    dialect:"mysql",
+
 }
+
+
+
+
+// export default {
+//     Host: "sql12.freesqldatabase.com",
+//     USER: "sql12677559",
+// PASSWORD: "FVhPjz6YkK",
+// DB:"sql12677559",
+// Port: 3306
+// }
